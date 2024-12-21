478d33214880749ab3c4f5a7aa410ed8bec6f8fc
FeliCres7 <48242703@est.ort.edu.ar>
2024-12-20 23:37:10 -0300

    .

---
+++ frontend/src/components/ListaBloques.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const ListaBloques = () => {
+  return (
+    <div>
+        <h1>Bloques:</h1>
+        <button>1° Bloque</button>
+        <button>2° Bloque</button>
+        <button>3° Bloque</button>
+        <button>4° Bloque</button>
+        <button>5° Bloque</button>
+    </div>
+  )
+}
